67ec78d266dcb13c1d80115ab8a225f1b47b24ee
Luke Chen <luke.chen.2010@gmail.com>
2020-05-31 21:20:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,4 +49,12 @@ class ApplicationController < Sinatra::Base
     
     redirect "/recipes/#{id}"
   end
+  
+  delete '/recipes/:id' do #destroy action
+    id = params[:id]
+    @recipe = Recipe.find_by_id(id)
+    @recipe.delete
+    
+  end
+  
 end
